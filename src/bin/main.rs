0b5da52f1c455a77c03a48fd22dbbe1145641672
d-ones <dunjones@gmail.com>
2026-01-04 11:28:37 -0500

    add embassy for rudimentary async

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,19 +5,50 @@ extern crate alloc;
 use esp_alloc::heap_allocator;
 
 use airqual::hardware_init::init_hardware;
+use airqual::hardware_init::AppHardware;
+use airqual::hardware_init::PreflightHardware;
 use airqual::sensor_reading::PlantowerFrame;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use embedded_graphics::{pixelcolor::Rgb565, pixelcolor::RgbColor, prelude::*};
 use esp_backtrace as _;
-
-use esp_hal::main;
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::timg::TimerGroup;
+use static_cell::StaticCell;
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[main]
-fn main() -> ! {
+// Managing peripherals across embassy timer loop + preflight init
+static HW: StaticCell<AppHardware> = StaticCell::new();
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
     heap_allocator!(size: 72 * 1024);
 
-    let (mut hw, mut state) = init_hardware();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+    let p = PreflightHardware {
+        i2c0: peripherals.I2C0,
+        spi2: peripherals.SPI2,
+        pin_display_pw: peripherals.GPIO7,
+        pin_backlight: peripherals.GPIO45,
+        pin_dc: peripherals.GPIO40,
+        pin_cs: peripherals.GPIO42,
+        pin_reset: peripherals.GPIO41,
+        pin_sck: peripherals.GPIO36,
+        pin_mosi: peripherals.GPIO35,
+        pin_miso: peripherals.GPIO37,
+        pin_sda: peripherals.GPIO3,
+        pin_scl: peripherals.GPIO4,
+    };
+    // Not wrestling with lifetime specifiers in preflight struct
+    let (hw_instance, mut state) = init_hardware(p);
+
+    let hw = HW.init(hw_instance);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
 
     loop {
         let mut i2c_buffer = [0u8; 32];
@@ -34,6 +65,6 @@ fn main() -> ! {
         hw.display.clear(Rgb565::BLACK).unwrap();
         state.render(&mut hw.display).unwrap();
 
-        hw.delay.delay_millis(1000);
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
--- src/hardware_init.rs
+++ src/hardware_init.rs
@@ -1,12 +1,15 @@
 use super::display::DisplayState;
 extern crate alloc;
 use alloc::boxed::Box;
+use embassy_time::Delay;
 use embedded_hal_bus::spi::{ExclusiveDevice, NoDelay};
 use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
-use esp_hal::delay::Delay;
+use esp_hal::gpio::Pin;
 use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::i2c::master::{Config as i2cConfig, I2c};
+use esp_hal::peripherals::{
+    GPIO3, GPIO35, GPIO36, GPIO37, GPIO4, GPIO40, GPIO41, GPIO42, GPIO45, GPIO7, I2C0, SPI2,
+};
 use esp_hal::spi::master::{Config, Spi};
 use esp_hal::spi::Mode;
 use esp_hal::time::Rate;
@@ -15,6 +18,21 @@ use mipidsi::interface::SpiInterface;
 use mipidsi::options::ColorOrder;
 use mipidsi::Display;
 use mipidsi::{models::ST7789, Builder};
+
+pub struct PreflightHardware<'a> {
+    pub i2c0: I2C0<'a>,
+    pub spi2: SPI2<'a>,
+    pub pin_display_pw: GPIO7<'a>,
+    pub pin_backlight: GPIO45<'a>,
+    pub pin_dc: GPIO40<'a>,
+    pub pin_cs: GPIO42<'a>,
+    pub pin_reset: GPIO41<'a>,
+    pub pin_sck: GPIO36<'a>,
+    pub pin_mosi: GPIO35<'a>,
+    pub pin_miso: GPIO37<'a>,
+    pub pin_sda: GPIO3<'a>,
+    pub pin_scl: GPIO4<'a>,
+}
 
 pub type TftDisplay<'a> = Display<
     SpiInterface<
@@ -26,42 +44,48 @@ pub type TftDisplay<'a> = Display<
     Output<'static>,
 >;
 
-pub struct AppHardware<'a> {
-    pub display: TftDisplay<'a>,
-    pub i2c: I2c<'a, Blocking>,
+pub struct AppHardware {
+    pub display: TftDisplay<'static>,
+    pub i2c: I2c<'static, Blocking>,
     pub delay: Delay,
 }
 
-pub fn init_hardware() -> (AppHardware<'static>, DisplayState) {
+pub fn init_hardware<'a>(p: PreflightHardware<'a>) -> (AppHardware, DisplayState) {
     // Init essential pieces of controller
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
-    let mut delay = Delay::new();
+    let mut delay = Delay;
     // let mut buffer = [0_u8; 4096];
     // Allocate on the heap and "leak" it so it becomes &'static mut [u8]
     let buffer = Box::leak(Box::new([0u8; 4096]));
 
     // Can't forget the backlight lol
-    let mut display_power = Output::new(peripherals.GPIO7, Level::High, OutputConfig::default());
+    let mut display_power = Output::new(
+        p.pin_display_pw.degrade(),
+        Level::High,
+        OutputConfig::default(),
+    );
     display_power.set_high();
 
-    let mut tft_backlight = Output::new(peripherals.GPIO45, Level::High, OutputConfig::default());
+    let mut tft_backlight = Output::new(
+        p.pin_backlight.degrade(),
+        Level::High,
+        OutputConfig::default(),
+    );
     tft_backlight.set_high();
 
     // Pins for display, direct current and chip select
-    let dc = Output::new(peripherals.GPIO40, Level::High, OutputConfig::default());
-    let cs = Output::new(peripherals.GPIO42, Level::High, OutputConfig::default());
+    let dc = Output::new(p.pin_dc.degrade(), Level::High, OutputConfig::default());
+    let cs = Output::new(p.pin_cs.degrade(), Level::High, OutputConfig::default());
 
     // Init SPI for display purposes
     let spi_config = Config::default()
         .with_frequency(Rate::from_mhz(3))
         .with_mode(Mode::_0);
 
-    let spi = Spi::new(peripherals.SPI2, spi_config)
+    let spi = Spi::new(p.spi2, spi_config)
         .unwrap()
-        .with_sck(peripherals.GPIO36)
-        .with_mosi(peripherals.GPIO35)
-        .with_miso(peripherals.GPIO37);
+        .with_sck(p.pin_sck.degrade())
+        .with_mosi(p.pin_mosi.degrade())
+        .with_miso(p.pin_miso.degrade());
 
     let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
 
@@ -71,7 +95,7 @@ pub fn init_hardware() -> (AppHardware<'static>, DisplayState) {
         .color_order(ColorOrder::Rgb)
         .invert_colors(mipidsi::options::ColorInversion::Inverted)
         .reset_pin(Output::new(
-            peripherals.GPIO41,
+            p.pin_reset.degrade(),
             Level::High,
             OutputConfig::default(),
         ))
@@ -82,20 +106,20 @@ pub fn init_hardware() -> (AppHardware<'static>, DisplayState) {
         .unwrap();
 
     let i2c = I2c::new(
-        peripherals.I2C0,
+        p.i2c0,
         i2cConfig::default().with_frequency(Rate::from_khz(100)),
     )
     .unwrap()
-    .with_sda(peripherals.GPIO3)
-    .with_scl(peripherals.GPIO4);
+    .with_sda(p.pin_sda.degrade())
+    .with_scl(p.pin_scl.degrade());
 
     (
         AppHardware {
-            display,
-            i2c,
-            delay,
+            // Hitting the special "I think my hardware won't get deallocated" button
+            display: unsafe { core::mem::transmute(display) },
+            i2c: unsafe { core::mem::transmute(i2c) },
+            delay: embassy_time::Delay,
         },
         DisplayState::new(),
     )
 }
-
